68a8c290a065b40ef4c1d0bddc991612a4e7dd27
cladams <cladams0203@gmail.com>
2019-08-13 09:47:47 -0400

    finished my personal card.  still have to do the following 5

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,9 +2,12 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-const data = axios.get('https://api.github.com/users/cladams0203')
+axios.get('https://api.github.com/users/cladams0203')
     .then(response => {
-        console.log(response)
+        const cards = document.querySelector('.cards')
+        cards.appendChild(
+        gitCard(response.data.avatar_url, response.data.name, response.data.login, response.data.location, response.data.html_url, response.data.followers, response.data.following, response.data.bio)
+        )
     })
     .catch(err => console.log(err))
 
@@ -35,13 +38,42 @@ function gitCard(image, name, username, location, github, followers, following, 
     cardContainer.classList.add('card')
     const userImage = document.createElement('img')
     userImage.src = image
+    const infoContainer = document.createElement('div')
+    infoContainer.classList.add('card-info')
     const heading = document.createElement('h3')
+    heading.classList.add('name')
     heading.textContent = name
+    const uName = document.createElement('p')
+    uName.classList.add('username')
+    uName.textContent = username
+    const userLocation = document.createElement('p')
+    userLocation.textContent = `Location: ${location}`
+    const profile = document.createElement('p')
+    profile.textContent = `Profile: `
+    const link = document.createElement('a')
+    link.setAttribute('href', github)
+    link.textContent = github
+    const follower = document.createElement('p')
+    follower.textContent = `Followers: ${followers}`
+    const follow = document.createElement('p')
+    follow.textContent = `Following: ${following}`
+    const userBio = document.createElement('p')
+    userBio.textContent = `Bio: ${bio}`
+    cardContainer.appendChild(userImage)
+    cardContainer.appendChild(infoContainer)
+    infoContainer.appendChild(heading)
+    infoContainer.appendChild(uName)
+    infoContainer.appendChild(userLocation)
+    infoContainer.appendChild(profile)
+    profile.appendChild(link)
+    infoContainer.appendChild(follower)
+    infoContainer.appendChild(follow)
+    infoContainer.appendChild(userBio)
 
-    console.log(cardContainer)
+    return cardContainer
 
 }
-gitCard()
+
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
 
